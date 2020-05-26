3615d0f1270f19cc80a7d9b22c5b4d5a063bb03b
Sebastian <sebastian.straszewski@gmail.com>
2020-05-26 10:11:17 +0200

    Add status file in /var/lib/fancontrol/

--- fancontrol.py
+++ fancontrol.py
@@ -4,13 +4,15 @@ import subprocess
 import time
 
 from gpiozero import OutputDevice
+from pathlib import Path
 
 
-ON_THRESHOLD = 65  # (degrees Celsius) Fan kicks on at this temperature.
-OFF_THRESHOLD = 55  # (degress Celsius) Fan shuts off at this temperature.
+ON_THRESHOLD = 55  # (degrees Celsius) Fan kicks on at this temperature.
+OFF_THRESHOLD = 45  # (degress Celsius) Fan shuts off at this temperature.
 SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
 GPIO_PIN = 17  # Which GPIO pin you're using to control the fan.
 
+Path('/var/lib/fancontrol').mkdir(parents=True, exist_ok=True)
 
 def get_temp():
     """Get the core temperature.
@@ -37,19 +39,25 @@ if __name__ == '__main__':
         raise RuntimeError('OFF_THRESHOLD must be less than ON_THRESHOLD')
 
     fan = OutputDevice(GPIO_PIN)
+    
+    with open('/var/lib/fancontrol/fancontrol_status', 'w') as state_file:
+        state_file.write('0')
 
     while True:
         temp = get_temp()
-
         # Start the fan if the temperature has reached the limit and the fan
         # isn't already running.
         # NOTE: `fan.value` returns 1 for "on" and 0 for "off"
         if temp > ON_THRESHOLD and not fan.value:
+            with open('/var/lib/fancontrol/fancontrol_status', 'w') as state_file:
+                state_file.write('1')
             fan.on()
 
         # Stop the fan if the fan is running and the temperature has dropped
         # to 10 degrees below the limit.
         elif fan.value and temp < OFF_THRESHOLD:
+            with open('/var/lib/fancontrol/fancontrol_status', 'w') as state_file:
+                state_file.write('0')
             fan.off()
 
         time.sleep(SLEEP_INTERVAL)
